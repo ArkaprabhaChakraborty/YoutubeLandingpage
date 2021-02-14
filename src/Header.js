d9ecda92c767f0fa1a8d2a5ffcff38a809850de4
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-02-15 03:17:12 +0530

    Headers stuffs

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuIcon from '@material-ui/icons/Menu'
 import SearchIcon from '@material-ui/icons/Search'
 import VideoCallIcon from'@material-ui/icons/VideoCall'
@@ -6,16 +6,25 @@ import AppsIcon from "@material-ui/icons/Apps"
 import NotificationsIcon from "@material-ui/icons/Notifications"
 import Avatar from "@material-ui/core/Avatar/Avatar"
 import './Header.css'
+import { Link } from 'react-router-dom'
 function Header() {
+    const [inputSearch, setInputSearch] = useState("");
     return (
         <div className="header">
             <div className="header_left">
                 <MenuIcon/>
+                <Link to="/">
                 <img className="header_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png" alt=""/>
+                </Link>
             </div>
             <div className="header_input">
-                <input placeholder="Search" type="text"/>
-                <SearchIcon className="header_inputbutton"/>
+                <input 
+                onChange={(e)=> setInputSearch(e.target.value)}
+                value={inputSearch}
+                placeholder="Search" type="text"/>
+                <Link to={`/search/${inputSearch}`}>
+                    <SearchIcon className="header_inputbutton"/>
+                </Link>
             </div>
             <div className="header_right">
                 <VideoCallIcon className="header_icon"/>
