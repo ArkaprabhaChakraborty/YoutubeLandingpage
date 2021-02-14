9e884081c151e6f90a99d0fc7a8645b3fc81cf6d
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-02-15 03:17:49 +0530

    Recommended.js edits

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,9 +1,84 @@
 import React from 'react'
 import './RecommendedVideos.css'
+import Video from'./Video.js'
 function RecommendedVideos() {
     return (
         <div className="recommendedvideos">
-            
+            <div className="recommendedvideos_videos">
+                <Video
+                    title="Something Really odd is up with Munarawar"
+                    views="2M Views"
+                    timestamp="4 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Munawar Farique"
+                    image="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <Video
+                    title="Something Really odd is up with Munarawar"
+                    views="2M Views"
+                    timestamp="4 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Munawar Farique"
+                    image="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <Video
+                    title="Something Really odd is up with Munarawar"
+                    views="2M Views"
+                    timestamp="4 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Munawar Farique"
+                    image="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <Video
+                    title="Something Really odd is up with Munarawar"
+                    views="2M Views"
+                    timestamp="4 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Munawar Farique"
+                    image="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <Video
+                    title="Something Really odd is up with Munarawar"
+                    views="2M Views"
+                    timestamp="4 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Munawar Farique"
+                    image="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <Video
+                    title="Something Really odd is up with Munarawar"
+                    views="2M Views"
+                    timestamp="4 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Munawar Farique"
+                    image="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <Video
+                    title="Something Really odd is up with Munarawar"
+                    views="2M Views"
+                    timestamp="4 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Munawar Farique"
+                    image="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <Video
+                    title="Something Really odd is up with Munarawar"
+                    views="2M Views"
+                    timestamp="4 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Munawar Farique"
+                    image="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                />
+                <Video
+                    title="Something Really odd is up with Munarawar"
+                    views="2M Views"
+                    timestamp="4 days ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Munawar Farique"
+                    image="https://yt3.ggpht.com/ytc/AAUvwniapyAQHeutpsdEAQOENjwxCuKykSS9gwIgsIhf6Q=s88-c-k-c0x00ffffff-no-rj"
+                />
+                
+            </div> 
         </div>
     )
 }
